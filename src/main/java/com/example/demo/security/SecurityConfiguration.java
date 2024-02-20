79234510ea091e41e117d36edbdfbbb23391a98f
nyoongoon <nyoongoon@naver.com>
2024-02-21 08:27:25 +0900

    SecurityConfig - 최신 버전에 맞춰서 구현

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -1,4 +1,60 @@
 package com.example.demo.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(HttpBasicConfigurer::disable) // -> rest api
+                .csrf(CsrfConfigurer::disable) // -> rest api
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  //jwt는 세션을 갖지 않음
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/**/signup", "/**/signin").permitAll()) // 아래의 web.ignoring()에서 관리 가능
+//                        .requestMatchers().hasRole() --> 어노테이션으로 처리 -> @EnableMethodSecurity
+//                .exceptionHandling(authenticationManager -> authenticationManager
+//                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+//                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
+                .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // antMatchers 부분도 deprecated 되어 requestMatchers로 대체
+        return web -> web.ignoring()
+                .requestMatchers("/h2-console/**");
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
