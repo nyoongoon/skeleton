42ed60d324d7b793afe418eba61c3f7df74262bb
nyoongoon <nyoongoon@naver.com>
2024-02-20 19:36:57 +0900

    토큰 생성 메소드

--- src/main/java/com/example/demo/security/TokenProvider.java
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -1,4 +1,36 @@
 package com.example.demo.security;
 
-public class TokenProvider{
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+    private static final long TOKEN_EXPIRED_TIME = 1000 * 60 * 60; //밀리세컨드*초*분 == 1시간
+    private static final String KEY_ROLES = "roles";
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, List<String> roles) {
+        // 사용자의 권한정보를 저장하기 위한 클레임 생성
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles); // 클레임은 키밸류
+        Date now = new Date();
+        Date expiredDate = new Date(now.getTime() + TOKEN_EXPIRED_TIME);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성 시간
+                .setExpiration(expiredDate) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512, secretKey) // 시그니처 알고리즘, 비밀키
+                .compact();
+    }
 }
