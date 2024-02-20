f89d3c7c9c590f363505f640d767b662847ab326
nyoongoon <nyoongoon@naver.com>
2024-02-20 18:36:37 +0900

    AppConfig 생성 - PasswordEncoder 구현 클래스 빈설정

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -8,26 +8,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService implements UserDetailsService {
+    private final PasswordEncoder passwordEncoder; //구현 클래스 설정 필요
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // Member 엔티티 UserDetails 상속하고 있음..
         return this.memberRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("에러 메시지 작성"));
+                .orElseThrow(() -> new UsernameNotFoundException("에러 메시지 작성"));
     }
 
-    public Member register(Auth.SignUp signUp){
+    public Member register(Auth.SignUp signUp) {
+        boolean exists = this.memberRepository.existsByUsername(signUp.getUsername());
+        if (exists) {
+            throw new IllegalStateException("이미 사용 중인 아이디입니다.");
+        }
+        // 패스워드 암호화
+
         return null;
     }
 
-    public Member authenticate(Auth.SignIn signIn){
+    public Member authenticate(Auth.SignIn signIn) {
+
         return null;
     }
 }
